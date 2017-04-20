58bfdec8542c77a88af455a644ec3034f9867075
Wesley Tansey <Wesley Tansey>
2017-04-20 08:07:40 -0500

    Added faster sdp

--- wavenet/sdp.py
+++ wavenet/sdp.py
@@ -114,10 +114,10 @@ class FastUnivariateSDP:
         if self._lam > 0:
             print 'neighborhoods:', self.neighborhoods
             neighbors = tf.transpose(tf.gather(self.neighborhoods, labels))
-            neighbor_logprobs = tf.map_fn(lambda n: self._node_logprobs(input_layer, n),
+            neighbor_logprobs = tf.map_fn(lambda n: tf.reduce_sum(self._node_logprobs(input_layer, n), axis=2),
                                           neighbors,
                                           dtype=tf.float32)
-            neighbor_logprobs = tf.transpose(neighbor_logprobs, [1,0,2])
+            neighbor_logprobs = tf.transpose(neighbor_logprobs, [1,0])
             print 'neighbor logprobs:', neighbor_logprobs
             regularizer = trend_filtering_penalty(neighbor_logprobs,
                                                   self.neighborhoods.get_shape()[1],
